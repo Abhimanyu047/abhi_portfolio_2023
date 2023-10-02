dd2c3760fc2d106a53c91ee8dc406ce5b745f913
Abhimanyu Sangitrao <abhi.sangitrao47@gmail.com>
2023-10-02 22:41:25 +0100

    Updates

---
+++ precache-manifest.92183fa86859a0e3a55f2e3647512cbc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "82c665afec1d2fbcc545",
+    "url": "/abhi_portfolio_2023/static/css/main.f3e11d9d.chunk.css"
+  },
+  {
+    "revision": "82c665afec1d2fbcc545",
+    "url": "/abhi_portfolio_2023/static/js/main.82c665af.chunk.js"
+  },
+  {
+    "revision": "4ee7637fd766474b00fc",
+    "url": "/abhi_portfolio_2023/static/css/1.56238545.chunk.css"
+  },
+  {
+    "revision": "4ee7637fd766474b00fc",
+    "url": "/abhi_portfolio_2023/static/js/1.4ee7637f.chunk.js"
+  },
+  {
+    "revision": "d2f1f967dc2b72223bd3",
+    "url": "/abhi_portfolio_2023/static/js/runtime~main.d2f1f967.js"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/abhi_portfolio_2023/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "fed0850a36ae8cfbe9ceaa1ce970c5cc",
+    "url": "/abhi_portfolio_2023/static/media/graph_embeddings.fed0850a.png"
+  },
+  {
+    "revision": "9c0f986795edbdc96c99bc8853580310",
+    "url": "/abhi_portfolio_2023/static/media/kafkaml.9c0f9867.png"
+  },
+  {
+    "revision": "4b632d51a45152af9b70077a3d4955ca",
+    "url": "/abhi_portfolio_2023/static/media/portfolio.4b632d51.png"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/abhi_portfolio_2023/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "23c7af6e8f4769d244281163aa485983",
+    "url": "/abhi_portfolio_2023/index.html"
+  }
+];
