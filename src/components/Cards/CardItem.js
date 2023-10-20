fb4f9219dbb6feed28ec81daa0aba15a19ba3f6e
Abhimanyu Sangitrao <abhi.sangitrao47@gmail.com>
2023-10-20 22:39:22 +0100

    routing error for projects fix 7: useNavigate used in anchor tag in CardItem

--- src/components/Cards/CardItem.js
+++ src/components/Cards/CardItem.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import CardIcons from "./CardIcons";
 import "./CardItem.css"
-
+import { useNavigate } from 'react-router-dom';
 const CardItem = (props) => {
     const paragraphStyle = {
         fontSize: "13px", // Set the font size to your desired value
         lineHeight: "1.5", // Optional: Set the line height if needed
         textAlign: 'center'
       };
-    
+    const navigate = useNavigate();
+    const handleLearnMoreClick = () => {
+    // Navigate to the URL specified in the "links" component
+    navigate(props.item.links[0].url);
+    };
 
     return (
 
@@ -22,10 +26,11 @@ const CardItem = (props) => {
             <div className={"card__body"}>
                 <h2 className={"card__title"}><strong>{props.item.title}</strong></h2>
                 <p style={paragraphStyle}>
-                    <a  href={props.item.links[0].url}
+                    {/* <a  href={props.item.links[0].url}
                         // target={"_blank"}
-                        rel={"noopener noreferrer"}>
-                    Click here to learn more..
+                        rel={"noopener noreferrer"}> */}
+                    <a onClick={handleLearnMoreClick}>
+                        Click here to learn more..
                     </a>
                 </p>
                 <p className={"card__description"}>{props.item.description}</p>
