ffa585805f4ced65d70c51dbd1192b786e2924a3
Abhimanyu Sangitrao <abhi.sangitrao47@gmail.com>
2023-10-23 15:01:49 +0100

    Project 2 conent organized properly in headings

--- src/assets/configs/ProjectPage2.js
+++ src/assets/configs/ProjectPage2.js
@@ -68,6 +68,30 @@ const ProjectPage2 = () => {
       </div>
       <div style={textStyle}>
         <p>
+          <b>Project Overview: </b> 
+          Analyzed the Tesco Grocery 1.0 dataset, comprising over 420 million food items purchased by 
+          1.6 million fidelity card owners in Greater London. The aim was to discover relationships 
+          between food habits, health, and economic factors.
+        </p>
+        <p>
+          <b>Data Analysis Process:</b>
+          <ul>
+            <li><b>Data Preparation and Exploration:</b> Imported essential Python libraries, including Pandas, NumPy, and Matplotlib. Loaded and preprocessed raw datasets related to grocery spending habits, childhood obesity prevalence, and household income levels across London. This involved handling missing values, formatting inconsistencies, and data type conversions. Conducted exploratory analysis through data aggregation, subsetting, and merging to create unified datasets for further statistical analysis.</li>
+            <li><b>Choropleth Visualizations:</b> Using the GeoPandas library, I generated choropleth visualizations to analyze regional consumption patterns and trends. These visualizations were instrumental in gaining insights into geographic variations in grocery spending and their potential impact on health and obesity levels.</li>
+            <li><b>Predictive Modeling and Correlation Analysis:</b> Predictive modeling techniques, including training a linear regression model, were employed to determine the relationship between food categories and obesity levels. Key model evaluation metrics, such as R-squared and mean squared error, were computed. Additionally, I calculated the Pearson correlation coefficient between grocery spending and income variables to uncover associations. Statistical hypothesis testing was used to identify significant correlations.</li>
+            <li><b>Data Visualization and Communication:</b> To effectively communicate the findings, I utilized the Matplotlib library to create insightful data visualizations, including clustered bar charts. These visualizations synthesized complex statistical insights into intuitive formats, making it easier for stakeholders to understand the analysis results.</li>
+          </ul>
+        </p>
+
+        <p>
+          <b>Conclusion: </b> 
+          In summary, this project involved a range of data science techniques, 
+          including data preparation, exploratory and predictive modeling, statistical analysis, 
+          and data visualization. The primary objective was to highlight intriguing relationships 
+          between food habits, health, and economic factors, providing actionable insights from the 
+          datasets.
+        </p>
+        {/* <p>
           The Tesco Grocery 1.0 dataset is a record of over 420 M food items, 
           purchased by 1.6M fidelity card owners across Greater London. The data analysis begins 
           by importing core Python libraries like Pandas, NumPy, and Matplotlib to enable data 
@@ -89,7 +113,7 @@ const ProjectPage2 = () => {
           predictive modeling, statistical analysis, and reporting are applied to derive actionable 
           insights from the datasets. The analysis aims to highlight interesting relationships between 
           food habits, health, and economic factors.
-        </p>
+        </p> */}
       </div>
     </div>
   );
